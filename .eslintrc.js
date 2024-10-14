ad827409375300a98848e0e21b67afeb8bcac9a9
Diogo Neto <168118296+DiogoNet0@users.noreply.github.com>
2024-10-14 22:27:36 +0100

    made a background gradient and added some functions to the operations of the calculator

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,15 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['airbnb-base'],
-  rules: { 'no-console': 'off' },
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'off',
+    'no-console': 'off',
+    'no-unused-vars': 'off',
+  },
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-};
+}
--- app.js
+++ app.js
@@ -1,30 +1,61 @@
-const btnAdd = document.getElementById('btn__key--addiction');
-const btnOne = document.getElementById('btn__key--one');
-const btnTwo = document.getElementById('btn__key--two');
-const btnThree = document.getElementById('btn__key--three');
-const btnFour = document.getElementById('btn__key--four');
-const btnFive = document.getElementById('btn__key--five');
-const btnSix = document.getElementById('btn__key--six');
-const btnSeven = document.getElementById('btn__key--seven');
-const btnEight = document.getElementById('btn__key--eight');
-const btnNine = document.getElementById('btn__key--nine');
-const displayResult = document.getElementById('display--result');
+const btnZero = document.getElementById('btn__key--zero')
+const btnOne = document.getElementById('btn__key--one')
+const btnTwo = document.getElementById('btn__key--two')
+const btnThree = document.getElementById('btn__key--three')
+const btnFour = document.getElementById('btn__key--four')
+const btnFive = document.getElementById('btn__key--five')
+const btnSix = document.getElementById('btn__key--six')
+const btnSeven = document.getElementById('btn__key--seven')
+const btnEight = document.getElementById('btn__key--eight')
+const btnNine = document.getElementById('btn__key--nine')
+const btnSub = document.getElementById('btn__key--subtraction')
+const btnMul = document.getElementById('btn__key--multiplication')
+const btnAdd = document.getElementById('btn__key--addiction')
+const btnDiv = document.getElementById('btn__key--division')
+const btnLog = document.getElementById('btn__key--log')
+const btnPercent = document.getElementById('btn__key--percentage')
+const btnAC = document.getElementById('btn__key--ac')
+const btnDec = document.getElementById('btn__key--decimal')
+const btnResult = document.getElementById('btn__key--result')
+const displayResult = document.getElementById('display--result')
 
-const result = 99;
+let firstOperand = ''
+let secondOperand = ''
+let currentOperation = null
+let result = 99
 
 function helloWorld() {
-  console.log('Hello world');
+  console.log('Hello world')
 }
 
-btnOne.addEventListener('click', helloWorld);
-btnTwo.addEventListener('click', helloWorld);
-btnThree.addEventListener('click', helloWorld);
-btnFour.addEventListener('click', helloWorld);
-btnFive.addEventListener('click', helloWorld);
-btnSix.addEventListener('click', helloWorld);
-btnSeven.addEventListener('click', helloWorld);
-btnEight.addEventListener('click', helloWorld);
-btnNine.addEventListener('click', helloWorld);
-btnAdd.addEventListener('click', helloWorld);
+// Operations
+// Addiction
+function add(a, b) {
+  return a + b
+}
 
-displayResult.textContent = result;
+// Substration
+function sub(a, b) {
+  return a - b
+}
+
+// Multiplication
+function mul(a, b) {
+  return a * b
+}
+
+// Division
+function div(a, b) {
+  return a / b
+}
+
+// Percentage
+function per(a, b) {
+  return (a * b) / 100
+}
+
+console.log(per(10, 2))
+// Log
+function log(a) {
+  return Math.log(a)
+}
