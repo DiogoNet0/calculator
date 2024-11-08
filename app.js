9a94c44b0badc3d97fe0d68e289925b8379114a4
Diogo Neto <168118296+DiogoNet0@users.noreply.github.com>
2024-11-08 12:50:39 +0000

    Remaking the logic of the calculator

--- app.js
+++ app.js
@@ -1,71 +1,92 @@
 // Variables
-const calcBtns = document.getElementById('calculator__buttons')
-const displayResult = document.getElementById('display--result')
-const displayCalc = document.getElementById('display--calculation')
+
 let firstOp = ''
 let secondOp = ''
 let operator = ''
 let equal = false
 let opResult = ''
 
+const numberButtons = document.querySelectorAll('[data-number]')
+const operatorButtons = document.querySelectorAll('[data-operator]')
+const acButton = document.getElementById('btn__key--ac')
+const equalButton = document.getElementById('btn__key--result')
+const dotButton = document.getElementById('btn__key--decimal')
+const displayResult = document.getElementById('display--result')
+const displayCalc = document.getElementById('display--calculation')
+
 function add(a, b) {
   return Number(a) + Number(b)
 }
 
-function operate() {
-  if (firstOp !== '' && operator !== '' && secondOp !== '') {
-    switch (operator) {
-      case 'addition':
-        opResult = add(firstOp, secondOp)
-        displayResult.textContent = opResult
-        // After calculation, set firstOp to the result for chaining operations
-        firstOp = opResult.toString()
-        secondOp = ''
-        operator = ''
-        console.log(opResult)
-        break
-      default:
-        console.log('Error')
-    }
-  }
+function sub(a, b) {
+  return Number(a) - Number(b)
 }
 
-function handleNumber(number) {
-  if (operator === '') {
-    firstOp += number
+function mul(a, b) {
+  return Number(a) * Number(b)
+}
+
+function div(a, b) {
+  if (a === 0 || b === 0) {
+    return 'Invalid Operation'
   } else {
-    secondOp += number
+    return Number(a) / Number(b)
   }
 }
+// function operate() {
+//   if (firstOp !== '' && operator !== '' && secondOp !== '') {
+//     switch (operator) {
+//       case 'addition':
+//         opResult = add(firstOp, secondOp)
+//         displayResult.textContent = opResult
+//         // After calculation, set firstOp to the result for chaining operations
+//         firstOp = opResult.toString()
+//         secondOp = ''
+//         operator = ''
+//         console.log(opResult)
+//         break
+//       default:
+//         console.log('Error')
+//     }
+//   }
+// }
 
-function handleOperation(operation) {
-  if (operation === 'equals') {
-    equal = true
-    console.log(equal)
-    operate()
-  } else {
-    operator = ''
-    operator += operation
-    console.log(operator)
-  }
-}
-
-function updateDisplay() {
-  if (secondOp === '') {
-    displayCalc.textContent = firstOp
-  } else {
-    displayCalc.textContent = secondOp
-  }
-}
-
-calcBtns.addEventListener('click', (event) => {
-  if (event.target.classList.contains('btn')) {
-    const value = event.target.getAttribute('data-value')
-    if (!isNaN(value)) {
-      handleNumber(value)
-    } else {
-      handleOperation(value)
-    }
-    updateDisplay()
-  }
-})
+// function handleNumber(number) {
+//   if (operator === '') {
+//     firstOp += number
+//   } else {
+//     secondOp += number
+//   }
+// }
+//
+// function handleOperation(operation) {
+//   if (operation === 'equals') {
+//     equal = true
+//     console.log(equal)
+//     operate()
+//   } else {
+//     operator = ''
+//     operator += operation
+//     console.log(operator)
+//   }
+// }
+//
+// function updateDisplay() {
+//   if (secondOp === '') {
+//     displayCalc.textContent = firstOp
+//   } else {
+//     displayCalc.textContent = secondOp
+//   }
+// }
+//
+// calcBtns.addEventListener('click', (event) => {
+//   if (event.target.classList.contains('btn')) {
+//     const value = event.target.getAttribute('data-value')
+//     if (!isNaN(value)) {
+//       handleNumber(value)
+//     } else {
+//       handleOperation(value)
+//     }
+//     updateDisplay()
+//   }
+// })
